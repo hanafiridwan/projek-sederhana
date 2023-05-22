931362341c2b1e5ae1c02d30ef7d2faa535af9ca
ridwanhanafi <ridwanacmad619@gmail.com>
2023-05-22 21:46:42 +0700

    Hasil coding

---
+++ PERTEMUAN SEMINAR/script.js
@@ -0,0 +1,23 @@
+// console.log("Apa lu Bego");
+
+
+// function greet(){
+//     console.log("Aing Maung");
+// }
+// greet();
+
+// console.log(Date()); //menampilkan tangaal waktu dan lokasi
+
+/* let x = 13;
+
+function example(){
+    let x = 10;
+    console.log(x);
+    if(true){
+        let x = 22;
+        console.log(x);
+    }
+}
+
+console.log(x);
+example(); */
